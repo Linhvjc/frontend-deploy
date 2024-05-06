ed3309a096dc74f66d995ba8f3fc9735578fbf1f
linhpn <phanphanvjc@gmail.com>
2024-05-06 19:43:51 +0700

    add

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "https://deploy-demo.onrender.com/api/v1";
+axios.defaults.baseURL = "https://linhpn-deploy-demo.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
